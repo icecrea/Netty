42e3db62f4ed24c8309b7cc6df1842434e388488
icecrea <798167096@qq.com>
2019-06-27 17:13:44 +0800

    'netty与websocket'

---
+++ src/main/java/com/example/cp11WebSocket/WebSocketServer.java
@@ -0,0 +1,53 @@
+package com.example.cp11WebSocket;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * @description:
+ * @author: icecrea
+ * @create: 2019-06-27 15:01
+ **/
+public class WebSocketServer {
+
+    public void run(final int port) throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast("http-codec", new HttpServerCodec());
+                            ch.pipeline().addLast("aggregator", new HttpObjectAggregator(65536));
+                            ch.pipeline().addLast("http-chunked", new ChunkedWriteHandler());
+                            ch.pipeline().addLast("handler", new WebSocketServerHandler());
+
+                        }
+                    });
+
+            Channel ch = b.bind(port).sync().channel();
+            System.out.println("web socket server started at port" + port);
+
+            ch.closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new WebSocketServer().run(8080);
+    }
+}
